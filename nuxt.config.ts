ea4862e0ea4ccfe3cc6708700b0b07aeaf258bf2
kickadnan <kickadnan@gmail.com>
2023-05-10 11:34:34 +0700

    update head

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -11,8 +11,8 @@ export default defineNuxtConfig({
     head: {
       charset: "utf-8",
       viewport: "width=device-width, initial-scale=1",
-      title: "Adnan F. Muhammad",
-      meta: [{ name: "description", content: "Website Personal Branding" }],
+      title: "Adnan F. Muhammad, Creative UI/UX Designer",
+      meta: [{ name: "description", content: "Experienced UI/UX designer with over a year of experience, I specialize in creating user-friendly interfaces for digital products in Indonesia and across the ASEAN region. Whether you need a reliable user interface designer in Indonesia or a user experience designer with a deep understanding of the ASEAN market, I can help. With in-depth knowledge of the latest design trends and user needs in this region, I can develop effective and efficient digital products that excel in design and functionality. As an expert in user-centered design, I am committed to creating user interfaces that are not only visually appealing but also easy to use. If you are looking for a skilled and experienced UI/UX designer in Indonesia or the ASEAN region, I am here to help." }],
       link: [{ rel: "icon", type: "image/png", href: "/favicon.png" }],
     },
   },
